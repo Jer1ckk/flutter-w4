c7ad6b3de8e7623441cbbe9afd873666fe3ccdcf
PovDavin <davin.pov@student.cadt.edu.kh>
2025-10-29 20:16:12 +0700

    v2

--- lib/ex3.dart
+++ lib/ex3.dart
@@ -5,60 +5,66 @@ void main() {
     MaterialApp(
       home: Container(
         padding: EdgeInsets.all(20),
-
         child: Column(
           children: [
-            CustomCard(title: "OOP", color: Colors.blue[100]!),
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.blue[100],
+              ),
+              child: Center(
+                child: Text(
+                  'OOP',
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    color: Colors.white,
+                    fontSize: 28,
+                  ),
+                ),
+              ),
+            ),
             SizedBox(height: 20),
-            CustomCard(
-              title: "OOP2",
-              gradient: LinearGradient(
-                colors: [Colors.blue[100]!, Colors.blue[600]!],
+            Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.blue[300],
+              ),
+              child: Center(
+                child: Text(
+                  'DART',
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    color: Colors.white,
+                    fontSize: 28,
+                  ),
+                ),
               ),
             ),
-            CustomCard(title: "Dart", color: const Color(0xFFFFE8A3)),
-            CustomCard(
-              title: "FLUTTER",
-              gradient: LinearGradient(
-                colors: [const Color.fromARGB(255, 241, 223, 168), Colors.amber],
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  colors: [Colors.blue[100]!, Colors.blue[900]!],
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  'FLUTTER',
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    color: Colors.white,
+                    fontSize: 28,
+                  ),
+                ),
               ),
             ),
-            CustomCard(title: 'title')
           ],
         ),
       ),
     ),
   );
-}
-
-class CustomCard extends StatelessWidget {
-  final String title;
-  final Color? color;
-  final Gradient? gradient;
-
-  const CustomCard({super.key, required this.title, this.color = Colors.blue, this.gradient});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        
-        gradient: gradient,
-        color: gradient == null ? color : null,
-      ),
-      child: Center(
-        child: Text(
-          title,
-          style: TextStyle(
-            decoration: TextDecoration.none,
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
 }
---
+++ lib/ex4.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            CustomCard(title: "OOP", color: Colors.blue[100]!),
+            CustomCard(title: "Dart", color: const Color(0xFFFFE8A3)),
+            CustomCard(
+              title: "FLUTTER",
+              gradient: LinearGradient(
+                colors: [const Color.fromARGB(255, 241, 223, 168), Colors.amber],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+class CustomCard extends StatelessWidget {
+  final String title;
+  final Color? color;
+  final Gradient? gradient;
+
+  const CustomCard({super.key, required this.title, this.color = Colors.blue, this.gradient});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        gradient: gradient,
+        color: gradient == null ? color : null,
+      ),
+      child: Center(
+        child: Text(
+          title,
+          style: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
+}
